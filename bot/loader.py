7991a5ec6ca674ce8ec05c2bcee2470790aff8d9
VasilevAlexandr97 <vasilev.alex.work@gmail.com>
2022-02-14 07:01:36 +0500

    add filters and generals handlers

--- bot/loader.py
+++ bot/loader.py
@@ -1,6 +1,7 @@
 from aiogram import Bot, Dispatcher, executor
 
 from bot.configs.config import config
+from bot.handlers.generals import register_generals_handlers
 from bot.middlewares.set_user import SetUserMiddleware
 from bot.models import connect_db, disconnect_db
 
@@ -20,6 +21,8 @@ def loader():
     dp = Dispatcher(bot)
 
     dp.setup_middleware(SetUserMiddleware())
+
+    register_generals_handlers(dp)
 
     executor.start_polling(dp, skip_updates=False, on_startup=on_startup,
                            on_shutdown=on_shutdown)
